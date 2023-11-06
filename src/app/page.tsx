a4a42caf617f98b7d1a24eb4cd5ab963d7e6bbdb
davidsanders <dsanders_m633@outlook.com>
2023-11-06 14:43:09 -0500

    created a logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,20 @@ export default function Home() {
     <main>
       <Col>
         <Row>
-          <Col className={"d-flex justify-content-center pt-5"}>
-            <p>
-              <i
-                className="fa-brands fa-wordpress"
-                style={{ fontSize: "10rem" }}
-              ></i>
-            </p>
+          <Col className={"pt-3 d-flex justify-content-center"}>
+            <div className={"d-flex align-items-center"}>
+              <Image
+                src="/Logo-Sandstack.webp"
+                alt={"StandStack"}
+                width={275}
+                height={275}
+              />
+              <div className={"ps-4"}>
+                <h1 style={{ fontSize: "4.5rem" }}>SandStack</h1>
+                <div className={"border-bottom border-3"}></div>
+                <p className={"pt-3"}>Nerd Free, Code Strong</p>
+              </div>
+            </div>
           </Col>
         </Row>
         <Row xs={1} md={2} lg={3} className="g-4 pt-5">
